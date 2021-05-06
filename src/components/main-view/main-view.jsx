091a67c725e44df94df43a476e78fc456b45fa99
cnjlv <79581768+cnjlv@users.noreply.github.com>
2021-05-06 15:56:02 +0200

    added MainView component and added new script

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export class MainView extends React.Component {
+
+  render() {
+    return (
+      <div className="main-view">
+        <div>How to Marry a Millionaire</div>
+        <div>Niagara</div>
+        <div>The Seven Year Itch</div>
+      </div>
+    );
+  }
+}
+
+export default MainView;
