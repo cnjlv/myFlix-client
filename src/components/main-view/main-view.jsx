96049ae376f85a42e16ffa891ec121418752d86d
cnjlv <79581768+cnjlv@users.noreply.github.com>
2021-05-10 06:40:42 +0200

    fetched movies from API

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
-import { MovieCard } from '../movie-card/movie-card.jsx';
+import axios from 'axios';
+
+import { LoginView } from '../login-view/login-view';
+import { RegistrationView } from '../registration-view/registration-view'
+import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+
+import "./main-view.scss";
 
 export class MainView extends React.Component {
 
   constructor() {
     super();
     this.state = {
-      movies: [
-        { _id: 1, Title: 'How to Marry a Millionaire', Description: 'How to Marry a Millionaire is a 1953 American romantic comedy film.', Genre: 'Comedy', Director: 'Jean Negulesco', ImagePath: 'https://m.media-amazon.com/images/M/MV5BZTM0MGU1MjgtMDJiMy00N2RkLWFiZDMtMmY5MGE5MGRmNzEwXkEyXkFqcGdeQXVyMDI2NDg0NQ@@._V1_UX182_CR0,0,182,268_AL_.jpg'},
-        { _id: 2, Title: 'Niagara', Description: 'As two couples are visiting Niagara Falls, tensions between one wife and her husband reach the level of murder.', Genre: 'Thriller', Director: 'Henry Hathaway', ImagePath: 'https://m.media-amazon.com/images/M/MV5BMDBhNWE4Y2UtMTA5Mi00YzYyLTliMWMtMjc3OGU0ZWFiMGVhL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_UX182_CR0,0,182,268_AL_.jpg'},
-        { _id: 3, Title: 'The Seven Year Itch', Description: 'When his family goes away for the summer, a hitherto faithful husband with an overactive imagination is tempted by a beautiful neighbbor.', Genre: 'Comedy', Director: 'Billy Wilder', ImagePath: 'https://m.media-amazon.com/images/M/MV5BZTJkMTBkNmYtYjdkZi00YjU1LWE5YWMtZGZhYWVmNzk4NzY3XkEyXkFqcGdeQXVyNDY2MTk1ODk@._V1_UX182_CR0,0,182,268_AL_.jpg'}
-      ],
-      selectedMovie: null
+      movies: [],
+      selectedMovie: null,
+      user: null,
+      register: null
     };
+  }
+
+  componentDidMount() {
+    axios.get('https://cnjlv.herokuapp.com/movies').then(response => {
+      this.setState({
+        movies: response.data
+      });
+    }).catch(error => {
+      console.log(error);
+    });
   }
 
   setSelectedMovie(newSelectedMovie) {
@@ -22,21 +36,20 @@ export class MainView extends React.Component {
     });
   }
 
-  render() {
+ render() {
     const { movies, selectedMovie } = this.state;
 
-    if (movies.length === 0) return <div className="main-view">This list is empty!</div>;
-
+    if (movies.length === 0) return <div className="main-view" />;
 
     return (
       <div className="main-view">
         {selectedMovie
-          ? <MovieView movie = {selectedMovie} onBackClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie); }} />
+          ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
           : movies.map(movie => (
-            <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />
-          ))
+            <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
+         ))
         }
       </div>
     );
   }
-} 
+}
