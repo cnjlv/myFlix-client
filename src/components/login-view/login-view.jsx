5c199cd27b3c50b6fb57faa744cb4833ee05e282
cnjlv <79581768+cnjlv@users.noreply.github.com>
2021-05-18 21:11:01 +0200

    added form validation and routing

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 import "./login-view.scss";
 
@@ -10,10 +11,14 @@ export function LoginView(props) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
+  const [usernameError, setUsernameError] = useState({});
+  const [passwordError, setPasswordError] = useState({});
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    /* Send a request to the server for authentication */
-    axios.post('https://cnjlv.herokuapp.com/users', {
+    const isValid = formValidation();
+    if (isValid) {
+    axios.post('https://cnjlv.herokuapp.com/login', {
       Username: username,
       Password: password
     })
@@ -24,6 +29,27 @@ export function LoginView(props) {
     .catch(e => {
       console.log('no such user')
     });
+    }
+  };
+
+  const formValidation = () => {
+    const usernameError = {};
+    const passwordError = {};
+    let isValid = true;
+
+    if (username.trim().length < 1) {
+      usernameError.usernameShort = "Username is required";
+      isValid = false;
+    }
+
+    if (password.trim().length < 1) {
+      passwordError.passwordMissing = "Password is required";
+      isValid = false;
+    }
+
+    setUsernameError(usernameError);
+    setPasswordError(passwordError);
+    return isValid;
   };
 
   return (
@@ -33,11 +59,32 @@ export function LoginView(props) {
         <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
       </Form.Group>
 
+      {Object.keys(usernameError).map((key) => {
+        return (
+          <div key={key}>
+            {usernameError[key]}
+          </div>
+        );
+      })}
+
       <Form.Group controlId="formPassword">
         <Form.Label>Password</Form.Label>
         <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
       </Form.Group>
-      <Button variant="dark" type="submit" onClick={handleSubmit}>Submit</Button>
+
+      {Object.keys(passwordError).map((key) => {
+      return (
+        <div key={key}>
+          {usernameError[key]}
+        </div>
+      );
+    })}
+
+      <Button variant="dark" type="submit" onClick={handleSubmit}>Login</Button>
+
+      <Link to={`/register`}>
+        <Button variant="dark" >New here? Register</Button>
+      </Link>
     </Form>
   );
 }
--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,19 +1,67 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export function RegistrationView(props) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
-  const [birthday, setBirthday] = useState('')
+  const [birthday, setBirthday] = useState('');
+
+  const [usernameError, setUsernameError] = useState({});
+  const [passwordError, setPasswordError] = useState({});
+  const [emailError, setEmailError] = useState({});
 
 
   const handleRegister = (e) => {
     e.preventDefault();
-    console.log(username, password, email, birthday)
+    console.log(username, password, email, birthday);
+    const isValid = formValidation();
+    if (isValid) {
+      axios.post('https://cnjlv.herokuapp.com/users', {
+        Username: username,
+        Password: password,
+        Email: email,
+        Birthday: birthday
+      }).then(response => {
+        const data = response.data;
+        console.log(data);
+        window.open('/', '_self');
+      }).catch(e => {
+        console.log('Registration failed')
+      });
+    }
   }
+  
+  const formValidation = () => {
+    const usernameError = {};
+    const passwordError = {};
+    const emailError = {};
+    let isValid = true;
+
+    if (username.trim().length < 5) {
+      usernameError.usernameShort = "Minimum 5 characters";
+      isValid = false;
+    }
+
+    if (password.trim().length < 1) {
+      passwordError.passwordMissing = "Password is required";
+      isValid = false;
+    }
+
+    if (!email.includes(".") && !email.includes("@")) {
+      emailError.emailNotEmail = "Email doesn't seem valid";
+      isValid = false;
+    }
+
+    setUsernameError(usernameError);
+    setPasswordError(passwordError);
+    setEmailError(emailError);
+    return isValid;
+  };
 
   return (
   <Form>
@@ -22,15 +70,39 @@ export function RegistrationView(props) {
         <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
       </Form.Group>
 
+      {Object.keys(usernameError).map((key) => {
+        return (
+          <div key={key}>
+            {usernameError[key]}
+          </div>
+        );
+      })}
+
       <Form.Group controlId="registerPassword">
         <Form.Label>Password</Form.Label>
         <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
       </Form.Group>
 
+      {Object.keys(passwordError).map((key) => {
+        return (
+          <div key={key}>
+            {usernameError[key]}
+          </div>
+        );
+      })}
+
       <Form.Group controlId="registerEmail">
         <Form.Label>Email</Form.Label>
         <Form.Control type="text" onChange={e => setEmail(e.target.value)} />
       </Form.Group>
+
+      {Object.keys(emailError).map((key) => {
+        return (
+          <div key={key}>
+            {emailError[key]}
+          </div>
+        );
+      })}
 
       <Form.Group controlId="registerBirthday">
         <Form.Label>Birthdate</Form.Label>
@@ -38,11 +110,16 @@ export function RegistrationView(props) {
       </Form.Group>
 
       <Button variant="dark" type="submit" onClick={handleRegister}>Register</Button>
+
+      
+      <Link to={`/`}>
+        <Button variant="dark">Login</Button>
+      </Link>
     </Form>
   );
 }
 
-RegistrationView.PropTypes = {
+RegistrationView.propTypes = {
   register: PropTypes.shape({
     Username: PropTypes.string.isRequired,
     Password: PropTypes.string.isRequired,
