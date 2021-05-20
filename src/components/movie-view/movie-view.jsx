a0d6924ef7d788f978389200b9ee6bf243bcb2fd
cnjlv <79581768+cnjlv@users.noreply.github.com>
2021-05-20 09:39:32 +0200

    added features add to favorite and added navbarview

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,14 +1,52 @@
 import React from 'react';
+import axios from 'axios';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
 import "./movie-view.scss";
+import {NavbarView} from "../navbar-view/navbar-view";
 
 export class MovieView extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  addFavorite(movie) {
+    let token = localStorage.getItem("token");
+    let url =
+      "https://cnjlv.herokuapp.com/users/" +
+      localStorage.getItem("user") +
+      "/movies/" +
+      movie._id;
+
+    console.log(token);
+
+    axios
+      .post(url, "", {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => {
+        console.log(response);
+        // window.open("/", "_self");
+        window.open("/users/" + localStorage.getItem("user"), "_self");
+        alert("Added to favorites!");
+      });
+  }
+
+
   render() {
     const { movie, onBackClick } = this.props;
+
     return (
-      <div className="movie-view">
+      <div>
+      <div>
+      <NavbarView />
+    </div>
+      <div className="movie-view" style={{
+                  marginTop: '70px',
+                }}>
         <div className="movie-image">
           <img src={movie.ImagePath} />
         </div>
@@ -23,30 +61,20 @@ export class MovieView extends React.Component {
         <Link to={`/directors/${movie.Director.Name}`}>
           <Button variant="dark">{movie.Director.Name}</Button>
         </Link>
+        <div>
+              <Button
+                variant="dark"
+                onClick={() => this.addFavorite(movie)}
+              >
+                Add to Favorites
+              </Button>
+            </div>
         <Link to={`/genres/${movie.Genre.Name}`}>
           <Button variant="dark">{movie.Genre.Name}</Button>
         </Link>
         <Button variant="dark" onClick={() => { onBackClick(null) }}>Back</Button>
       </div>
+      </div>
     );
-  }
+    }
 }
-
-MovieView.propTypes = {
-  movie: PropTypes.shape({
-    Title: PropTypes.string.isRequired,
-    Description: PropTypes.string.isRequired,
-    ImagePath: PropTypes.string.isRequired,
-    Genre: PropTypes.shape({
-      Name: PropTypes.string.isRequired,
-      Description: PropTypes.string.isRequired
-    }),
-    Director: PropTypes.shape({
-      Name: PropTypes.string.isRequired,
-      Bio: PropTypes.string.isRequired,
-      Birth: PropTypes.string.isRequired,
-      Death: PropTypes.string
-    }),
-  }).isRequired,
-  onBackClick: PropTypes.func.isRequired
-};
