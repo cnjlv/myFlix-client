599241bf7cd2dcc6ed84c8fd5ea72fcdb7450149
cnjlv <79581768+cnjlv@users.noreply.github.com>
2021-05-28 06:27:10 +0200

    styled director-view in a separate file

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -3,6 +3,7 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 import {NavbarView} from "../navbar-view/navbar-view";
+import "./director-view.scss";
 
 
 export class DirectorView extends React.Component {
@@ -13,42 +14,22 @@ export class DirectorView extends React.Component {
         <div>
       <NavbarView />
       </div>
-      <div className="director-view" style={{
-                  marginTop: '70px',
-                  width: '100%',
-                }}>
-        <div className="director-name" style={{
-                  fontSize: '25px',
-                  display: 'flex',
-                  justifyContent: 'center',
-                  marginBottom: '5px',
-                }}>
+      <div className="director-view">
+        <div className="director-name">
           <span className="value">{director.Name}</span>
         </div>
         <div className="director-bio">
           <span className="value">{director.Bio}</span>
         </div>
-        <div className="director-birth" style={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                  marginBottom: '5px',
-                }}>
+        <div className="director-birth">
           <span className="label">Birth: </span>
           <span className="value">{director.Birth}</span>
         </div>
-        <div className="director-death" style={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                  marginBottom: '5px',
-                }}>
+        <div className="director-death">
           <span className="label">Death: </span>
           <span className="value">{director.Death}</span>
         </div>
-        <div style={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                  marginTop: '25px',
-                }}>
+        <div className="button">
         <Button variant="dark" onClick={() => { onBackClick(null) }}>Back</Button>
         </div>
       </div>
